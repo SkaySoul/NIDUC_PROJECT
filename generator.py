84dba81a2be86010a50c0f4fe9e7109cedfe5472
SkaySoul <hellfireltd@gmail.com>
2021-05-18 13:40:33 +0200

    cezarScrumble w końcu działa

--- generator.py
+++ generator.py
@@ -3,32 +3,29 @@ from zaklocenia import negationScrambler
 from zaklocenia import repeatScrambler
 from zaklocenia import cezarScramble
 
+
 def generator(size, ones_p, packet_size):
+    zeros = int(size) - round(int(size) * int(ones_p) / 100, 0)
+    ones = int(size) - int(zeros)
 
-  zeros = int(size) - round(int(size) * int(ones_p)/100,0)
-  ones=int(size)-int(zeros)
-
-  data = [0]*int(zeros) + [1]*int(ones)
-  shuffle(data)
-  print (data)
-  #save(packet_size, data)
-  #negationScrambler(data)
-  #print(data)
-  #repeatScrambler(data)
-  cezarScramble(data, packet_size)
-  save(packet_size, data)
-  print(data)
-
+    data = [0] * int(zeros) + [1] * int(ones)
+    shuffle(data)
+    print(data)
+    # save(packet_size, data)
+    # negationScrambler(data)
+    # print(data)
+    # repeatScrambler(data)
+    cezarScramble(data, packet_size)
+    save(packet_size, data)
+    print(data)
 
 
 def save(packet_size, data):
-  file = open("packages.txt", "w+")   # "w+"- do zapisu i odczytu
+    file = open("packages.txt", "w+")  # "w+"- do zapisu i odczytu
 
-  for index in range(1, len(data)+1):
-    file.write(str(data[index-1]))
-    if index % int(packet_size) == 0:
-     file.write("\n")
+    for index in range(1, len(data) + 1):
+        file.write(str(data[index - 1]))
+        if index % int(packet_size) == 0:
+            file.write("\n")
 
-  file.close() 
-
-
+    file.close()
--- zaklocenia.py
+++ zaklocenia.py
@@ -1,66 +1,62 @@
 def negationScrambler(data):
     index = 0
-    while(index < len(data)):
-      if(data[index] == 0):
-        data[index] = 1
+    while index < len(data):
+        if data[index] == 0:
+            data[index] = 1
 
-      elif(data[index] == 1):
-        data[index] = 0
+        elif data[index] == 1:
+            data[index] = 0
 
-      index += 2  
-      
+        index += 2
 
 
 def cezarScramble(data, packet_size):
-  key = 3;
-  range_var = int(packet_size)-1
-  
-  for range_index in range(0, len(data)-range_var):
-    for index in range (0, range_var):
-      data_temp = list()
-      data_temp.append(data[index+range_index]) 
-    for index2 in range (0, range_var):
-      temp = index2+key-1
-      if(temp>range_var):
-        temp-=range_var
-      if(range_index+index2<len(data)):
-        data[(range_index+index2)-1] = data_temp[temp-1]
-    range_index+=range_var  
-      
+    key = 3
+    range_var = int(packet_size)
+
+    for range_index in range(0, len(data) - range_var, range_var):
+        data_temp = list()
+        for index in range(0, range_var):
+            data_temp.append(data[index + range_index])
+        for index2 in range(0, range_var):
+            temp = index2 + key
+            if temp > range_var:
+                temp -= range_var
+            if range_index + index2 < len(data):
+                data[(range_index + index2)-1] = data_temp[temp-1]
+
+
 
 def repeatScrambler(data):
-  reps_0 = 0
-  reps_1 = 0
-  for index in range(0, len(data)):
-    if data[index] == 0:
-      reps_0 += 1
-      reps_1 = 0
+    reps_0 = 0
+    reps_1 = 0
+    for index in range(0, len(data)):
+        if data[index] == 0:
+            reps_0 += 1
+            reps_1 = 0
 
-      if reps_0 == 3:
-        data[index] = 1
-        reps_0 = 0 
+            if reps_0 == 3:
+                data[index] = 1
+                reps_0 = 0
 
-    if data[index] == 1:
-      reps_1 += 1
-      reps_0 = 0
+        if data[index] == 1:
+            reps_1 += 1
+            reps_0 = 0
 
-      if reps_1 == 3:
-        data[index] = 0
-        reps_1 = 0
+            if reps_1 == 3:
+                data[index] = 0
+                reps_1 = 0
 
-    
 
 def negationDescrambler(data):
     index = 0
-    while(index < len(data)):
-      if(data[index] == 0):
-        data[index] = 1
+    while index < len(data):
+        if data[index] == 0:
+            data[index] = 1
 
-      elif(data[index] == 1):
-        data[index] = 0
+        elif data[index] == 1:
+            data[index] = 0
 
-      index += 2  
+        index += 2
 
-
-
-#def repeatDescrambler(data):
+    # def repeatDescrambler(data):
