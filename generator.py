789adc96a5b3be93c82e153c3740a53670b6de0a
matCh00 <mateusz.chalik.g@gmail.com>
2021-06-10 19:34:46 +0200

    lepsza struktura + komentarze

--- generator.py
+++ generator.py
@@ -4,10 +4,12 @@ from zaklocenia import negationScramble, negationDescramble
 from zaklocenia import shiftScramble, shiftDescramble
 from zaklocenia import multiplicativeScramble, multiplicativeDescramble
 from zaklocenia import additiveScramble, additiveDescramble
-from analiza import repeatCounter, stats
+from analiza import stats
+from odbiornik import packetDetector
 
 
-def generator(size, ones_p, packet_size):
+# główna funkcja sterująca programem
+def generate(size, ones_p, packet_size):
     zeros = int(size) - round(int(size) * int(ones_p) / 100, 0)
     ones = int(size) - int(zeros)
 
@@ -16,18 +18,17 @@ def generator(size, ones_p, packet_size):
         data.append(0)
     for j in range(0, int(ones)):
         data.append(1)
-    # data = [0] * int(zeros) + [1] * int(ones)
+
     shuffle(data)
     print(data)
-    # save(packet_size, data)
 
     # negationScramble(data)
     # print(data)
     # negationDescramble(data)
 
-    #shiftScramble(data, packet_size)
-    #print(data)
-    #shiftDescramble(data, packet_size)
+    # shiftScramble(data, packet_size)
+    # print(data)
+    # shiftDescramble(data, packet_size)
 
     multiplicativeScramble(data)
     print(data)
@@ -39,21 +40,8 @@ def generator(size, ones_p, packet_size):
 
     # save(packet_size, data)
 
-    # repeatCounter(data, packet_size)
+    # packetDetector(data, packet_size)
 
     print(data)
 
     stats(data, packet_size)
-
-    
-
-
-def save(packet_size, data):
-    file = open("packages.txt", "w+")
-
-    for index in range(1, len(data) + 1):
-        file.write(str(data[index - 1]))
-        if index % int(packet_size) == 0:
-            file.write("\n")
-
-    file.close()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from generator import generator
+from generator import generate
 
 size = input("długość sygnału: ")
 ones_p = input("ilość jedynek(procentowo): ")
 packet_size = input("długość pakietów: ")
 
-generator(size, ones_p, packet_size)
+generate(size, ones_p, packet_size)
